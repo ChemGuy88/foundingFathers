ed9843b1bf593af192f46007611f223dda89fe89
Herman <hf.autore@gmail.com>
2022-12-03 21:01:51 -0500

    Update script.py
    I realized James Wood mentioned both declaration of independence and constitution, possibly conflating them.

--- script.py
+++ script.py
@@ -208,10 +208,15 @@ print(summary)
 # in my opinion. At least one founding father was 26 years old at signing, quite an 
 # accomplishment for that age. In the original data we see that James Monroe, the 4th
 # US president, was 18 at the age of signing, however, he himself did not sign it,
-# according to the data from the cited websited. Actor James Wood claimed in his
-# interview that James Monroe had signed the constitution.
-#      Nevertheless, the data do corroborate Woods' claim on the relative age
-# of the founding fathers, as 44 is younger than 60, and definitely younger than 
+# according to the data from the cited websited.
+# 
+# Actor James Wood brought up James Monroe when talking about how some people complain
+# about how the constitution was written by "a bunch of old white men". It seems his
+# statemenet is poorly structured because the declaration of independence and the
+# US constitution are two different things. On top of that the US constitution is
+# the sequel to its original incarnation, the Articles of Confederation. However,
+# there is a kernel of reason in James Woods broaching of the subject; the relative age
+# of the founding fathers is fairly young: 44 is younger than 60, and definitely younger than 
 # either Trump or Biden when either first became president in 2016 and 2020,
 # respectively.
 
